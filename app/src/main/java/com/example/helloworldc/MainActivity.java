ddc8d6ed7b0370c31311af2250a79c1a464b88a2
Yanny Esteban <yannyesteban@hotmail.com>
2019-06-10 14:55:00 -0400

    ok

---
+++ app/src/main/java/com/example/helloworldc/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.helloworldc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Button boton = (Button)
+                findViewById(R.id.button);
+        boton.setText("Click a un boton sin controlar la fuente del click");
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Song_detail = new Intent(getApplicationContext(), android.app.NativeActivity.class);
+                startActivity(Song_detail);
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
